e746e4b5c07c7efc11b63afc8b1a150b88b551bb
acechua <acecyq@gmail.com>
2018-08-10 18:29:25 +0800

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # devise_for :views
+  devise_for :users
+  devise_scope :user do
+  	get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'orders#index'
