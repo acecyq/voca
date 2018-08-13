7c63dfdf40861d604f777ad4e6105c03a0d18561
acechua <acecyq@gmail.com>
2018-08-13 14:58:06 +0800

    add twilio and dotenv

--- Gemfile
+++ Gemfile
@@ -63,3 +63,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Twilio
+gem 'twilio-ruby', '~> 5.12.1'
+
+# ENV variables
+gem 'dotenv-rails', groups: [:development, :test]
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   	get '/users/sign_out' => 'devise/sessions#destroy'
   end
   resources :orders
+  get '/orders/:id/sms', to: 'orders#sms', as: 'sms_order'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'orders#index'
 end
