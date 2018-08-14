fb7fecb51df18ed9a801178fa192daaa36f529e3
acechua <acecyq@gmail.com>
2018-08-14 10:56:34 +0800

    ignore .env file. add homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   devise_scope :user do
   	get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :orders
+  get '/orders/home', to: 'orders#home', as: 'home_orders'
   get '/orders/:id/sms', to: 'orders#sms', as: 'sms_order'
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'orders#index'
+  root 'orders#home'
 end
