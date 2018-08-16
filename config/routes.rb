518d41cf4bfa146b6ed7414c9005c24e2be37b6b
acechua <acecyq@gmail.com>
2018-08-16 17:16:32 +0800

    add qr code and more styling

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   end
   get '/orders/home', to: 'orders#home', as: 'home_orders'
   get '/orders/:id/sms', to: 'orders#sms', as: 'sms_order'
+  post '/orders/call', to: 'orders#call', as: 'call_order'
   resources :orders
+  get '/users/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'orders#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+orders = [
+	["cc x 1", "Yopapa", "91827582", 1],
+	["bl x 1", "Yomama", "91827582", 1],
+	["ss x 1", "DaWae", "91827582", 1],
+	["ccd x 1", "Yolanda", "91827582", 2],
+	["mr with egg x 1", "Anakin", "91827582", 2],
+	["cs x 1", "Shakira", "91827582", 2]
+]
+
+orders.each do |description, name, number, user_id|
+	Order.create(
+		description: description, 
+		name: name,
+		number: number,
+		user_id: user_id
+	)
+end
